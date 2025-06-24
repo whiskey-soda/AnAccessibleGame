3806b8bf4a635e7f197804cd823ee3ee80f2c611
ben <bmfboom9@gmail.com>
2025-06-23 19:58:32 -0600

    improve ground detection + tune jump more

--- anaccessiblegame/Assets/Scripts/Grounded.cs
+++ anaccessiblegame/Assets/Scripts/Grounded.cs
@@ -6,19 +6,19 @@ public class Grounded : MonoBehaviour
 
     [SerializeField] LayerMask Ground;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.layer == 7)
-        {
-            isGrounded = true;
-        }
-    }
-
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.layer == 7)
         {
             isGrounded = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 7)
+        {
+            isGrounded = true;
         }
     }
 
