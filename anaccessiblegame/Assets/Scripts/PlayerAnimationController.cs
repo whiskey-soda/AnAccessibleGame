b2b44c9d549e7c9532bbf827e802218c0b4eec10
ben <bmfboom9@gmail.com>
2025-07-18 10:26:11 -0600

    add speed-based animation speed

--- anaccessiblegame/Assets/Scripts/PlayerAnimationController.cs
+++ anaccessiblegame/Assets/Scripts/PlayerAnimationController.cs
@@ -4,19 +4,37 @@ public class PlayerAnimationController : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb2d;
     [SerializeField] Grounded grounded;
+    [SerializeField] Movement movement;
     [SerializeField] Animator animator;
 
     private void Update()
     {
-        if (rb2d.linearVelocityX > 0.1f) { transform.localScale = new Vector2(1, transform.localScale.y); }
-        else if (rb2d.linearVelocityX < -0.1f) { transform.localScale = new Vector2(-1, transform.localScale.y); }
+        // flip sprite to face move direction
+        if (rb2d.linearVelocityX > 0.1f) { FaceSprite(1); }
+        else if (rb2d.linearVelocityX < -0.1f) { FaceSprite(-1); }
 
+        animator.speed = 1;
 
         if (grounded.isGrounded && rb2d.linearVelocityX == 0) { animator.Play("Idle"); }
 
-        else if (grounded.isGrounded && rb2d.linearVelocityX != 0) { animator.Play("Walk"); }
+        else if (grounded.isGrounded && rb2d.linearVelocityX != 0) 
+        { 
+            animator.Play("Walk");
+
+            // animation speed depends on how fast player is moving
+            animator.speed = Mathf.Abs(rb2d.linearVelocityX) / movement.maxMoveSpeed;
+        }
 
         else if (!grounded.isGrounded) { animator.Play("Jump_Static"); }
+    }
+
+    public void FaceSprite(int direction)
+    {
+        if (direction > 0)
+        {
+            transform.localScale = new Vector2(1, transform.localScale.y);
+        }
+        else if (direction < 0) { transform.localScale = new Vector2(-1, transform.localScale.y); }
     }
 
 }
